b1d2eb8cf5b9123f55278f2c535682986d613903
Faith <felominofaith@gmail.com>
2026-03-31 20:20:16 +0800

    barrel

--- unifiedsystem/src/components/index.js
+++ unifiedsystem/src/components/index.js
@@ -1,6 +1,8 @@
+import Home from './Home'
+import Navbar from './Navbar'
+import First from './First'
+import Second from './Second'
+import Third from './Third'
 
-export { default as Home } from "./Home";
-export { default as Navbar } from "./Navbar";
-export { default as First } from "./First";
-export { default as Second } from "./Second";
-export { default as Third } from "./Third";
+
+export {Home, Navbar, First, Second, Third}
