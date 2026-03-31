4f2ec312145359abbaf79ab242e5247de1457516
Faith <felominofaith@gmail.com>
2026-03-31 19:03:12 +0800

    nav links working

--- unifiedsystem/src/components/Navbar.jsx
+++ unifiedsystem/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+        <ul>
+            <Link>First</Link>
+            <Link>Second</Link>
+            <Link>Third</Link>
+        </ul>
+    </nav>
   )
 }
