99112afd79c458af9c77eb77e0c70ac56be6b735
Faith <felominofaith@gmail.com>
2026-03-31 19:11:57 +0800

    pages routing

--- unifiedsystem/src/App.jsx
+++ unifiedsystem/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar'
+import { RouterContextProvider, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/first' element={<First />} />
+        <Route path='/second' element={<Second />} />
+        <Route path='/third' element={<Third />} />
+      </Routes>
     </>
   )
 }
--- unifiedsystem/src/components/Navbar.jsx
+++ unifiedsystem/src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import {Link} from 'react-router-dom'
 export const Navbar = () => {
   return (
     <nav className='navbar'>
+        
         <ul>
-            <Link>First</Link>
-            <Link>Second</Link>
-            <Link>Third</Link>
+            <Link to="/home">Home</Link>
+           <li> <Link to="/first">First</Link></li>
+           <li> <Link to="/second">Second</Link> </li>
+           <li> <Link to="/third">Third</Link> </li>
         </ul>
     </nav>
   )
