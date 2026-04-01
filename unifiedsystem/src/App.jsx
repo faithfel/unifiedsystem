11429c9010257fb0500e9b257372aee4046f214f
Faith <felominofaith@gmail.com>
2026-04-01 19:23:56 +0800

    IM SO GOATED

--- unifiedsystem/src/App.jsx
+++ unifiedsystem/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { RouterContextProvider, Routes } from 'react-router-dom'
+import { Route, RouterContextProvider, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { Home } from './components/Home'
 import { POS } from './components/POS'
@@ -21,6 +21,16 @@ function App() {
       <Navbar />
       <Routes >
         <Route path="/" element={<Home />} />
+        <Route path="/pos" element={<POS />} />
+        <Route path="/transactions" element={<Transaction />} />
+        <Route path="/stocks" element={<Stocks />} />
+        <Route path="/employees" element={<EmployeesList />} />
+        <Route path="/departments" element={<Departments />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/leave" element={<Leave />} />
+        <Route path="/payroll" element={<Payroll />} />
+        <Route path="/categories" element={<Categories />} />
+        
 
       </Routes>
     </>
