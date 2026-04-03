dbd25bc4f6ac43417beddbd80781c05d3fe8f880
Faith <felominofaith@gmail.com>
2026-04-03 16:30:29 +0800

    im losing it

--- unifiedsystem/src/components/Stocks.jsx
+++ unifiedsystem/src/components/Stocks.jsx
@@ -14,7 +14,7 @@ export const Stocks = () => {
   const handleSubmit = (event) => {
     event.preventDefault(); 
 
-    axios.post('http://localhost:3306/api/save', inputs)
+    axios.post('http://localhost:3306/api/user/saves', inputs)
     console.log(inputs);
   }
 
