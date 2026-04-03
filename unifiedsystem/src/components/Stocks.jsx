58bdbf26ed8c07d4f976017f3ad586cca9518596
Faith <felominofaith@gmail.com>
2026-04-03 11:57:09 +0800

    form

--- unifiedsystem/src/components/Stocks.jsx
+++ unifiedsystem/src/components/Stocks.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 export const Stocks = () => {
   return (
     <div className='maincontent'>
-      Stocks
+      <h1>Stocks</h1>
+
+      <form>
+
+        <label>Product</label>
+        <input type="text" name="product" />
+
+        <label>SKU</label>
+        <input type="text" name="sku" />
+
+        <label>Category</label>
+        <input type="text" name="category" />
+
+        <label>Price</label>
+        <input type="text" name="price" />
+
+        <label>Stock</label>
+        <input type="text" name="stock" />
+
+        <label>Status</label>
+        <input type="text" name="status" />
+
+        <button type="save">Save</button>
+
+
+      </form>
     </div>
   )
 }
