2981d3eea39a1a697b4c98afd214978cb48e8b31
Faith <felominofaith@gmail.com>
2026-04-09 13:01:58 +0800

    fix

---
+++ unifiedsystem/src/components/ListStock.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+
+export const ListStocks = () => {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost/api/api.php').then(res => setStocks(res.data))
+    .catch(err => console.log(err));
+  }, []);
+  
+
+  
+  return (
+    <div className='liststock'>
+      <h1>Stocks List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Product</th>
+            <th>SKU</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Status</th>
+          </tr>
+          
+        </thead>
+        <tbody>
+          {stocks.map((stock, key) =>
+            <tr key={key}> 
+              <td>{stock.id}</td>
+              <td>{stock.product}</td>
+              <td>{stock.sku}</td>
+              <td>{stock.category}</td>
+              <td>{stock.price}</td>
+              <td>{stock.stock}</td>
+              <td>{stock.status}</td>
+              <td>
+                <Link to={`stock/${stock.id}/edit`}>Edit</Link>
+                <button>Delete</button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+
+}
+
+export default ListStocks
--- unifiedsystem/src/components/Stocks.jsx
+++ unifiedsystem/src/components/Stocks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -55,54 +55,5 @@ export const Stocks = () => {
   )
 }
 
-
-export const ListStocks = () => {
-  const [stocks, setStocks] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost/api/api.php').then(res => setStocks(res.data))
-    .catch(err => console.log(err));
-  }, []);
-  
-
-  
-  return (
-    <div className='liststock'>
-      <h1>Stocks List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Product</th>
-            <th>SKU</th>
-            <th>Category</th>
-            <th>Price</th>
-            <th>Stock</th>
-            <th>Status</th>
-          </tr>
-          
-        </thead>
-        <tbody>
-          {stocks.map((stock, key) =>
-            <tr key={key}> 
-              <td>{stock.id}</td>
-              <td>{stock.product}</td>
-              <td>{stock.sku}</td>
-              <td>{stock.category}</td>
-              <td>{stock.price}</td>
-              <td>{stock.stock}</td>
-              <td>{stock.status}</td>
-              <td>
-                <Link to={`stock/${stock.id}/edit`}>Edit</Link>
-                <button>Delete</button>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
-
-}
 
 export default Stocks
