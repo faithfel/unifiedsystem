3e0ed1d6a581568d92db4c91d1c5b5d46a6106a5
Faith <felominofaith@gmail.com>
2026-04-09 13:42:46 +0800

    Update ListStocks.jsx

--- unifiedsystem/src/components/ListStocks.jsx
+++ unifiedsystem/src/components/ListStocks.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 
 export const ListStocks = () => {
-  const [stocks, setStocks] = useState([]);
+  const [data, setStocks] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost/api/api.php').then(res => setStocks(res.data))
@@ -32,20 +32,19 @@ export const ListStocks = () => {
 
               </thead>
               <tbody>
-                  {stocks.map((stock, key) => <tr key={key}>
-                      <td>{stock.id}</td>
-                      <td>{stock.product}</td>
-                      <td>{stock.sku}</td>
-                      <td>{stock.category}</td>
-                      <td>{stock.price}</td>
-                      <td>{stock.stock}</td>
-                      <td>{stock.status}</td>
-                      <td>
-                          <Link to={`stock/${stock.id}/edit`}>Edit</Link>
-                          <button>Delete</button>
-                      </td>
-                  </tr>
-                  )}
+                  {
+                    data.map((stock) => (
+                      <tr key={stock.id}>
+                        <td>{stock.id}</td>
+                        <td>{stock.product}</td>
+                        <td>{stock.sku}</td>
+                        <td>{stock.category}</td>
+                        <td>{stock.price}</td>
+                        <td>{stock.stock}</td>
+                        <td>{stock.status}</td>
+                      </tr>
+                    ))
+                  }
               </tbody>
           </table>
       </div>
